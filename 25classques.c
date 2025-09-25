b79bdd26dcd0166c3019381f32ffe8c2492c080f
ujjwalgupta5r-dev <ujjwalgupta5r@gmail.com>
2025-09-25 17:22:18 +0530

    Add files via upload

---
+++ 25classques.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int factorial(int n) {
+    if (n == 0 || n == 1) 
+    return 1;
+    else
+    return factorial(n - 1) * n ;  
+}
+
+int main() {
+    int num, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial of a negative number doesn't exist.\n");
+    } else {
+        result = factorial(num);
+        printf("Factorial of %d = %d\n", num, result);
+    }
+
+    return 0;
+}
